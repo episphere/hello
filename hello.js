72001200454be08774f1daa00b0424b144b7dbfe
Jonas Almeida <jalmeida@mathbiol.org>
2019-06-26 22:41:22 -0400

    disqus is on

--- hello.js
+++ hello.js
@@ -4,10 +4,11 @@ window.onload=async _=>{
     let info = await (await fetch('https://us-central1-nih-nci-dceg-episphere-dev.cloudfunctions.net/helloMe')).json()
     let h='<h2>About You</h2>'
     h += '<p>'
-    h += `Full information below, with the details about your machine, your browser, the languages you command, your address (machine, postal address and maybe even <a href="https://www.google.com/maps?q=${info.aboutYou.headers['x-appengine-citylatlong']}" target="_blank" style="background-color:yellow">GPS coordinates</a>) under "headers" and "rawHeaders". See how much of this is noyt provided when you use the browser in incognito mode.`
+    h += `Full information below, with the details about your machine, your browser, the languages you use when browsing, your address (machine, postal address and maybe even <a href="https://www.google.com/maps?q=${info.aboutYou.headers['x-appengine-citylatlong']}" target="_blank" style="background-color:yellow">GPS coordinates</a>) under "headers" and "rawHeaders". See how much of this is not provided when you use the browser in incognito mode.`
     h += '</p>'
     h += '<pre id="rawInfo"></pre>'
+    h +=
     aboutYou.innerHTML=h
     rawInfo.innerHTML=JSON.stringify(info,null,3)
-    debugger
+    //debugger
 }
