b0aa268264430307d74e30472a09216c48e682ce
Jonas Almeida <jalmeida@mathbiol.org>
2019-06-26 22:31:37 -0400

    ready for disqus

--- hello.js
+++ hello.js
@@ -1,1 +1,13 @@
 console.log('hello.js loaded')
+
+window.onload=async _=>{
+    let info = await (await fetch('https://us-central1-nih-nci-dceg-episphere-dev.cloudfunctions.net/helloMe')).json()
+    let h='<h2>About You</h2>'
+    h += '<p>'
+    h += `Full information below, with the details about your machine, your browser, the languages you command, your address (machine, postal address and maybe even <a href="https://www.google.com/maps?q=${info.aboutYou.headers['x-appengine-citylatlong']}" target="_blank" style="background-color:yellow">GPS coordinates</a>) under "headers" and "rawHeaders". See how much of this is noyt provided when you use the browser in incognito mode.`
+    h += '</p>'
+    h += '<pre id="rawInfo"></pre>'
+    aboutYou.innerHTML=h
+    rawInfo.innerHTML=JSON.stringify(info,null,3)
+    debugger
+}
